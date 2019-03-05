4ddbe048647b3766ec5b3596ca892185deb31dd7
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-03-05 22:13:19 +0545

    resolve 'WSGIRequest' object has no attribute 'account' exception: get savings account to operate on directly via the submmited form instead of request

--- transactions/views.py
+++ transactions/views.py
@@ -7,22 +7,22 @@ from .forms import SavingDepositForm,SavingWithdrawlForm
 
 def saving_deposit_view(request):
     template = 'transactions/savings_form.html'
+
     form = SavingDepositForm(request.POST or None)
 
     if form.is_valid():
         deposit = form.save(commit=False)
-        deposit.account = request.account
         # adds deposit to the users saving account current balance
         deposit.account.current_balance += deposit.amount
         deposit.account.save()
         deposit.save()
-        messages.success(request, 'You Have Deposited Rs. {} only.'
-                         .format(deposit.amount))
-        return redirect("home")
+        messages.success(request, 'You Have successfully Deposited Rs. {} only to the account number {}.'
+                         .format(deposit.amount,deposit.account.owner.mem_number))
+        return redirect("saving_transaction:deposit")
 
     context = {
         'form': form,
-        'title': "Savings Deposit"
+        'title': "Deposit"
     }
 
     return render(request, template, context)
