6718ef7a73e99342f31452eb8e964f1590f9f40d
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-03-05 17:38:30 +0545

    create saving deposit model create saving withdrawl model register saving withdrawl and deposit model make and apply migrations for transactions app

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import (SavingDeposit,SavingWithdrawl)
 
 # Register your models here.
+
+admin.site.register(SavingDeposit)
+admin.site.register(SavingWithdrawl)
