f7e16317f51135181a66758c95f46c9a4783e4f1
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-03-05 16:15:18 +0545

    create members model add members app in installed apps make and apply migrations for members app

--- members/models.py
+++ members/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    mem_number = models.PositiveIntegerField(unique=True)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    address = models.CharField(max_length=256)
+    contact = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.first_name
+
