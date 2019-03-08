04bd7117e357819347411811db6301859a7bfc87
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-03-08 13:14:05 +0545

    create LoanAccount model register LoanAccount model make and apply migations for loans app

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from members.models import Member
 
 # Create your models here.
+
+class LoanAccount(models.Model):
+    owner = models.OneToOneField(Member, on_delete=models.CASCADE)
+    total_principal = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.owner.first_name
