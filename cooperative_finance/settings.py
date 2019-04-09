f28e408ff3c03fbd4032b3bbd63a4deaab47e1f2
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-04-10 00:34:44 +0545

    add heroku app domain to the allowed_hosts

--- cooperative_finance/settings.py
+++ cooperative_finance/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('CO_FINANCE_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cooperative-finance.herokuapp.com']
 
 
 # Application definition
