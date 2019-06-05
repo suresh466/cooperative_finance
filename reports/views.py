525d389365057ace3d58086dbbc224fe865988a0
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-06-05 13:45:46 +0545

    create functions to get required data for the report templates adjust report context names to avoid collision in monthly report template in the future

--- reports/views.py
+++ reports/views.py
@@ -11,71 +11,89 @@ def report(request):
 
     return render(request, template)
 
+def _income():
+    incomes = Income.objects.all()
+    return incomes
+
 def income(request):
     template = 'reports/income.html'
 
-    incomes = Income.objects.all()
-
     context = {
-            'items': incomes,
-            'title': "Income",
-            }
+        'items_income': _income(),
+        'title_income': 'Income',
+        }
 
     return render(request, template, context)
+
+def _expense():
+    expenses = Expense.objects.all()
+    return expenses
 
 def expense(request):
     template = 'reports/expense.html'
 
-    expenses = Expense.objects.all()
-
     context = {
-            'items': expenses,
-            'title': "Expense",
+            'items_expenses': _expenses(),
+            'title_expenses': "Expense",
             }
 
     return render(request, template, context)
+
+def _loan():
+    loans_rec = LoanPayment.objects.all()
+    loans_issued = LoanIssue.objects.filter(status='Approved')
+    return {'loans_rec': loans_rec, 'loans_issued': loans_issued}
 
 def loan(request):
     template = 'reports/loans.html'
 
-    loans_rec = LoanPayment.objects.all()
-    loans_issued = LoanIssue.objects.filter(status='Approved')
+    items = _loan()
 
     context = {
-            'items': loans_rec,
-            'issued_items': loans_issued,
-            'title': 'Loans',
+            'items_loans': items.get("loans_rec","none"),
+            'issued_items_loans': items.get("loans_issued","none"),
+            'title_loans': 'Loans',
             }
 
     return render(request, template, context)
 
-def capital(request):
-    template = 'reports/capital.html'
+def _capital():
 
     shares_buy = ShareBuy.objects.all()
-    shares_buy_sum = shares_buy.aggregate(Sum('number'))['number__sum']
     shares_sell = ShareSell.objects.all()
+    shares_buy_sum = shares_buy.aggregate(Sum('number'))['number__sum']
     shares_sell_sum = shares_sell.aggregate(Sum('number'))['number__sum']
 
     savings_deposit = SavingDeposit.objects.all()
-    savings_deposit_sum = savings_deposit.aggregate(Sum('amount'))['amount__sum']
     savings_withdrawal = SavingWithdrawal.objects.all()
+    savings_deposit_sum = savings_deposit.aggregate(Sum('amount'))['amount__sum']
     savings_withdrawal_sum= savings_withdrawal.aggregate(Sum('amount'))['amount__sum']
 
     loan_payment = LoanPayment.objects.all()
-    loan_payment_sum= loan_payment.aggregate(Sum('principal'))['principal__sum']
     loan_issued = LoanIssue.objects.filter(status='Approved')
+    loan_payment_sum= loan_payment.aggregate(Sum('principal'))['principal__sum']
     loan_issued_sum= loan_issued.aggregate(Sum('principal'))['principal__sum']
 
+    return {'shares_buy_sum': shares_buy_sum, 'shares_sell_sum': shares_sell_sum,
+	'savings_deposit_sum': savings_deposit_sum, 'savings_withdrawal_sum':savings_withdrawal_sum,
+	'loan_payment_sum': loan_payment_sum,'loan_issued_sum': loan_issued_sum,
+  	 }
+
+
+
+def capital(request):
+    template = 'reports/capital.html'
+
+    items = _capital()
+
     context = {
-            'shares_buy_sum': shares_buy_sum,
-            'shares_sell_sum': shares_sell_sum,
-            'savings_deposit_sum': savings_deposit_sum,
-            'savings_withdrawal_sum': savings_withdrawal_sum,
-            'loan_payment_sum': loan_payment_sum,
-            'loan_issued_sum': loan_issued_sum,
-            'loan_issued': loan_issued,
-            'title': 'Capital',
+            'shares_buy_sum_capital': items.get('shares_buy_sum','none'),
+            'shares_sell_sum_capital': items.get('shares_sell_sum','none'),
+            'savings_deposit_sum_capital': items.get('savings_deposit_sum','none'),
+            'savings_withdrawal_sum_capital': items.get('savings_withdrawal_sum','none'),
+            'loan_payment_sum_capital': items.get('loan_payment_sum','none'),
+            'loan_issued_sum_capital': items.get('loan_issued_sum','none'),
+            'title_capital': 'Capital',
             }
 
     return render (request, template, context)
