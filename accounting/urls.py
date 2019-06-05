47cac1ab8f6dcd6b96cced0ba169ec5c2e924eb6
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-06-05 22:03:13 +0545

    add income expense delete view do not show deleted transactions in accounting

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
 from .views import (income,expense,
-                    accounting,)
+                    accounting,income_delete,
+                    expense_delete,
+                    )
 
 app_name = 'accounting'
 urlpatterns = [
         path('', accounting, name="accounting"),
         path('income/', income, name="income"),
         path('expense/', expense, name="expense"),
+        path('income/<int:pk>/delete/', income_delete, name="income_delete"),
+        path('expense/<int:pk>/delete/', expense_delete, name="expense_delete"),
 ]
