a069cfbaae4009d717115fc2a004e7cf0a850e9e
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-06-06 12:55:43 +0545

    add status field to member model and set it to activated by default

--- members/models.py
+++ members/models.py
@@ -2,12 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+ACCOUNT_STATUS_CHOICE = (
+    ('Deactivated','Deactivated'),
+    ('Activated', 'Activated'),
+    )
+
 class Member(models.Model):
     mem_number = models.PositiveIntegerField(unique=True)
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
     address = models.CharField(max_length=256)
     contact = models.CharField(max_length=10)
+    status = models.CharField(choices=ACCOUNT_STATUS_CHOICE, default='Activated', max_length=11,editable=False)
 
     def __str__(self):
         return self.first_name
