8a82e6da39db523ef7bcc756b9c5ec0d7fbb7e59
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-06-12 18:46:14 +0545

    add date_created and date_updated fields to all savings models

--- savings/models.py
+++ savings/models.py
@@ -7,20 +7,21 @@ from django.db.models.signals import post_save
 # Create your models here.
 
 ACCOUNT_STATUS_CHOICE = (
-            ('Deactivated', 'Deactivated'),
-            ('Activated', 'Activated'),
-            )
+        ('Deactivated', 'Deactivated'),
+        ('Activated', 'Activated'),
+        )
 
 DELETE_STATUS_CHOICE = (
-            ('False', 'False'),
-            ('True', 'True'),
-            )
-
+        ('False', 'False'),
+        ('True', 'True'),
+        )
 
 class SavingAccount(models.Model):
     owner = models.OneToOneField(Member, on_delete=models.CASCADE)
     current_balance = models.PositiveIntegerField()
     status = models.CharField(choices=ACCOUNT_STATUS_CHOICE, default='Deactivated', max_length=11)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.owner.first_name
@@ -29,6 +30,8 @@ class SavingDeposit(models.Model):
     account = models.ForeignKey(SavingAccount, on_delete=models.CASCADE)
     amount = models.PositiveIntegerField()
     delete_status = models.CharField(choices=DELETE_STATUS_CHOICE, default='False', max_length=5, editable=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.account.owner.first_name
@@ -37,6 +40,8 @@ class SavingWithdrawal(models.Model):
     account = models.ForeignKey(SavingAccount, on_delete=models.CASCADE)
     amount = models.PositiveIntegerField()
     delete_status = models.CharField(choices=DELETE_STATUS_CHOICE, default='False', max_length=5, editable=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.account.owner.first_name
