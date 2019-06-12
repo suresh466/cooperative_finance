1f6ac98993778ec397db3baa0bd58f249e9928ff
Suresh Thagunna <suresh.thagunna77@gmail.com>
2019-06-12 18:37:44 +0545

    add date_created and date_updated fields to all loans models display date created in loans report

--- loans/models.py
+++ loans/models.py
@@ -6,26 +6,26 @@ from django.db.models.signals import post_save
 # Create your models here.
 
 STATUS_CHOICE = (
-    ('Pending', 'Pending'),
-    ('Approved', 'Approved'),
-    )
+        ('Pending', 'Pending'),
+        ('Approved', 'Approved'),
+        )
 
 ACCOUNT_STATUS_CHOICE = (
-       	    ('Deactivated', 'Deactivated'),
-            ('Activated', 'Activated'),
-            )
+       	('Deactivated', 'Deactivated'),
+        ('Activated', 'Activated'),
+        )
 
 DELETE_STATUS_CHOICE = (
-    ('False', 'False'),
-    ('True', 'True'),
-    )
-
-
+        ('False', 'False'),
+        ('True', 'True'),
+        )
 
 class LoanAccount(models.Model):
     owner = models.OneToOneField(Member, on_delete=models.CASCADE)
     total_principal = models.PositiveIntegerField()
     status = models.CharField(choices=ACCOUNT_STATUS_CHOICE, default='Deactivated', max_length=11)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.owner.first_name
@@ -36,6 +36,8 @@ class LoanIssue(models.Model):
     principal = models.PositiveIntegerField()
     status = models.CharField(choices=STATUS_CHOICE, default='Pending', max_length=15)
     delete_status = models.CharField(choices=DELETE_STATUS_CHOICE, default='False', max_length=5, editable=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.account.owner.first_name
@@ -44,6 +46,8 @@ class LoanPayment(models.Model):
     loan_num = models.ForeignKey(LoanIssue, on_delete=models.CASCADE)
     principal = models.PositiveIntegerField()
     delete_status = models.CharField(choices=DELETE_STATUS_CHOICE, default='False', max_length=5, editable=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.loan_num.account.owner.first_name
